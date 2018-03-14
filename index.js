6638fe6fc45e4f87d22ee8c30ed56ea7b4771032
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-03-14 18:17:11 +0530

    updating

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 var syncs = {};
 
+exports.format = function (str) {
+    var re = /(%?)(%([jds]))/g;
+    var args = Array.prototype.slice.call(arguments, 1);
+    if (args.length) {
+        str = str.replace(re, function (match, escaped, ptn, flag) {
+            var arg = args.shift();
+            switch (flag) {
+                case 's':
+                    arg = '' + arg;
+                    break;
+                case 'd':
+                    arg = Number(arg);
+                    break;
+                case 'j':
+                    arg = JSON.stringify(arg);
+                    break;
+            }
+            if (!escaped) {
+                return arg;
+            }
+            args.unshift(arg);
+            return match;
+        })
+    }
+    if (args.length) {
+        str += ' ' + args.join(' ');
+    }
+    str = str.replace(/%{2,2}/g, '%');
+    return '' + str;
+};
+
 exports.sync = function (id, run, done) {
     if (syncs[id]) {
         return syncs[id].push(done);
@@ -16,22 +47,28 @@ exports.sync = function (id, run, done) {
 };
 
 exports.configs = function (name, done) {
-    $.ajax({
-        method: 'GET',
-        url: exports.resolve('accounts://apis/v/configs/' + name),
-        dataType: 'json',
-        success: function (config) {
-            done(false, config.value);
-        },
-        error: function () {
-            console.log('error retrieving ' + name);
-            done('error retrieving ' + name + ' configuration');
-        }
-    });
+    exports.sync('configs:' + name, function (ran) {
+        $.ajax({
+            method: 'GET',
+            url: exports.resolve('accounts://apis/v/configs/' + name),
+            dataType: 'json',
+            success: function (config) {
+                ran(false, config.value);
+            },
+            error: function () {
+                console.log('error retrieving ' + name);
+                ran('error retrieving ' + name + ' configuration');
+            }
+        });
+    }, done);
 };
 
 exports.id = function () {
     return Math.random().toString(36).slice(2);
+};
+
+exports.clone = function (o) {
+    return JSON.parse(JSON.stringify(o));
 };
 
 exports.resolve = function (url) {
@@ -47,4 +84,48 @@ exports.resolve = function (url) {
     var sub = protocol.replace('://', '');
     var suffix = url.substring(protocol.length);
     return server.replace('{sub}', sub) + '/' + suffix;
+};
+
+var event = function (listeners, event) {
+    return listeners[event] || (listeners[event] = {on: [], once: []});
+};
+
+var EventEmitter = function () {
+    this.listeners = {};
+};
+
+EventEmitter.prototype.on = function (name, fn) {
+    event(this.listeners, name).on.push(fn);
+};
+
+EventEmitter.prototype.once = function (name, fn) {
+    event(this.listeners, name).once.push(fn);
+};
+
+EventEmitter.prototype.off = function (name, fn) {
+    var arr = event(this.listeners, name);
+    var idx = arr.on.indexOf(fn);
+    if (idx !== -1) {
+        arr.on.splice(idx, 1);
+    }
+    idx = arr.once.indexOf(fn);
+    if (idx !== -1) {
+        arr.once.splice(idx, 1);
+    }
+};
+
+EventEmitter.prototype.emit = function (name, data) {
+    var o = event(this.listeners, name);
+    var args = Array.prototype.slice.call(arguments, 1);
+    o.on.forEach(function (fn) {
+        fn.apply(fn, args);
+    });
+    o.once.forEach(function (fn) {
+        fn.apply(fn, args);
+    });
+    o.once = [];
+};
+
+exports.eventer = function () {
+    return new EventEmitter();
 };
